ebeee822376f952e8ee0657bb34ec3beaf2c62e0
Joseph Molina <jomolina@csumb.edu>
2017-06-13 19:46:25 -0700

    Initial commit

---
+++ JMineSweeper.java
@@ -0,0 +1,17 @@
+/**
+ * Created by josephmolina on 6/13/17.
+ */
+public class JMineSweeper {
+
+    public static void displayWelcomeMessage(){
+
+        System.out.println("Welcome to JMineSweeper." + "\n"
+        + "Enter and X and Y coordinate to select the square.");
+
+    }
+    public static void main(String[] args){
+
+        displayWelcomeMessage();
+    }
+
+}
